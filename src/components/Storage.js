6afa08319f5bca3c5caf1a116eec4ca03f34b895
Gintare <gintaar3@gmail.com>
2020-12-01 14:33:22 +0100

    Storage + images

--- src/components/Storage.js
+++ src/components/Storage.js
@@ -1,12 +1,24 @@
-
-
 function Storage(props) {
+  return (
+    <article className="storage-wrapper">
+      <h1>Storage</h1>
+      {props.apiData.map((storage) => {
+        const imageUrl = storage.name.replace(/\s+/g, "").toLowerCase();
 
-	return (
-		<article className="storage-wrapper">
-			<h1>Storage</h1>
-		</article>
-	)
+        return (
+          <div className="storage-card">
+            <div className="storage-left-col">
+              <img src={`images/${imageUrl}.png`}></img>
+            </div>
+            <div className="storage-right-col">
+              <span>{storage.name}</span>
+              <h3>{storage.amount}</h3>
+            </div>
+          </div>
+        );
+      })}
+    </article>
+  );
 }
 
-export default Storage
+export default Storage;
